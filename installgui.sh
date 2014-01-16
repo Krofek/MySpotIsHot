ac7bf53a329b2f3bf5779b13517ebf75fed93a44
Krofek <vrabec.matej@gmail.com>
2014-01-16 11:54:14 +0100

    updated installgui.sh

--- installgui.sh
+++ installgui.sh
@@ -50,7 +50,7 @@ if [[ $installer = "Y" || $installer = "y" ]]; then
     wget https://raw.github.com/Krofek/MySpotIsHot/master/country-list-iso-codes.txt
 	sudo cp MySpotIsHotGUI.sh /usr/bin/myspotishot
 	cp myspoticon.jpg ~/.myspot/myspoticon.jpg
-    cp country-list-iso-codes.txt ~/.myspot/
+    sudo cp country-list-iso-codes.txt /root/.myspot/
 	sudo rm -rf ~/.local/share/applications/myspotishot.desktop
 	mkdir ~/.local/share/applications
 	echo -e -n "[Desktop Entry]\nComment=Setup and create a WiFi AP\nTerminal=false\nName=MySpotIsHot\nExec=pkexec myspotishot\nType=Application\nIcon=~/.myspot/myspoticon.jpg\nCategories=Internet;" > ~/.local/share/applications/myspotishot.desktop
